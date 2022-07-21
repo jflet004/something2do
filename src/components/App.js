60f694e791a395a05cde6433c154975a57fd3f59
Jose Fletes <joseangelfletes@gmail.com>
2022-07-21 08:29:42 -0700

    import rest of components to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import './App.css'
 import { Switch, Route } from 'react-router-dom'
+import Header from './Header'
+import NavBar from './NavBar'
+import ActivityContainer from './ActivityContainer'
+import Footer from './Footer'
+import ActivityForm from './ActivityForm'
+
 
 const App = () => {
   return (
     <div>
-  
+      <NavBar />
+      <Header />
+      <Switch>
+        <Route>
+
+        </Route>
+      </Switch>
     </div>
   )
 }
