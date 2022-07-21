f279dfaf5bd7bd330738f62e098f643b05072187
Jose Fletes <joseangelfletes@gmail.com>
2022-07-21 09:32:05 -0700

    add About component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
-import React from 'react'
 import './App.css'
-import { Switch, Route } from 'react-router-dom'
+import React from 'react'
 import Home from './Home'
 import Header from './Header'
 import NavBar from './NavBar'
 import ActivityContainer from './ActivityContainer'
 import Footer from './Footer'
 import ActivityForm from './ActivityForm'
+import About from './About'
+import { Switch, Route } from 'react-router-dom'
 
 const App = () => {
   return (
@@ -27,6 +28,10 @@ const App = () => {
 
         <Route path="/activityform">
           <ActivityForm />
+        </Route>
+
+        <Route path="/about">
+          <About />
         </Route>
       </Switch>
       <Footer />
