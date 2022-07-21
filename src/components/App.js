d2a84e142f055cdfe682c0a7b31202e6c4c3846b
Jose Fletes <joseangelfletes@gmail.com>
2022-07-21 08:35:43 -0700

    create routes for components

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,32 @@
 import React from 'react'
 import './App.css'
 import { Switch, Route } from 'react-router-dom'
+import Home from './Home'
 import Header from './Header'
 import NavBar from './NavBar'
 import ActivityContainer from './ActivityContainer'
 import Footer from './Footer'
 import ActivityForm from './ActivityForm'
 
-
 const App = () => {
   return (
     <div>
-      <NavBar />
       <Header />
+      <NavBar />
       <Switch>
-        <Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
 
+        <Route exact path="/activities">
+          <ActivityContainer />
+        </Route>
+
+        <Route exact path="/addactivity">
+          <ActivityForm />
         </Route>
       </Switch>
+      <Footer />
     </div>
   )
 }
