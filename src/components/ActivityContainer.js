225e1ce9a4abb6136973017ca609c0e97ecf0119
Jose Fletes <joseangelfletes@gmail.com>
2022-07-21 09:54:57 -0700

    fetch data and save in state

--- src/components/ActivityContainer.js
+++ src/components/ActivityContainer.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import ActivityFilter from "./ActivityFilter"
+import ActivityList from "./ActivityList"
 
 const ActivityContainer = () => {
+
+  const [activities, setActivities] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3001/activities")
+    .then(r => r.json())
+    .then(data => setActivities(data))
+    .catch(error => alert(error))
+  },[])
+
   return (
     <div>
-      
+      <ActivityFilter />
+      <ActivityList 
+        activities={activities}
+      />
+
     </div>
   )
 }
