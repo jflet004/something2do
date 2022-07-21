ff74ee45fae4c8f8fb751ed2e5bbff4d04e1de6c
Jose Fletes <joseangelfletes@gmail.com>
2022-07-21 11:32:21 -0700

    create controlled form

--- src/components/ActivityForm.js
+++ src/components/ActivityForm.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ActivityForm = () => {
+
+  const [title, setTitle] = useState("")
+  const [type, setType] = useState("")
+  const [price, setPrice] = useState("")
+  const [participants, setParticipants] = useState("")
+  const [link, setLink] = useState("")
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    const activityObj = {
+      title,
+      type,
+      price,
+      participants,
+      link,
+    }
+    console.log(activityObj)
+
+    fetch("http://localhost:3001/activities", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(activityObj)
+    })
+    // .then(r => r.json())
+    // .then(data => console.log(data))
+  }
+
   return (
     <div>
-      
+      <form onSubmit={handleSubmit}>
+        <label>*Title</label>
+        <input required onChange={(e) => setTitle(e.target.value)} type="text" name="title" id="title" value={title}></input><br /><br />
+        <label>*Type</label>
+        <input required onChange={(e) => setType(e.target.value)} type="text" name="type" id="type" value={type}></input><br /><br />
+        <label>*Price</label>
+        <input required onChange={(e) => setPrice(e.target.value)} type="text" name="price" id="price" value={price}></input><br /><br />
+        <label>*Participants</label>
+        <input required onChange={(e) => setParticipants(e.target.value)} type="text" name="participants" id="participants" value={participants}></input><br /><br />
+        <label>Link</label>
+        <input onChange={(e) => setLink(e.target.value)} type="text" name="link" id="link" value={link}></input><br /><br />
+        <p>* required fields</p>
+        <input type="submit" value="Add"></input><br /><br />
+      </form>
     </div>
   )
 }
